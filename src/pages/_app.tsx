8674394efb4ed5e8c7cdac980231c76b3e78cb76
jeonkyungwon <ashjkw3218@gmail.com>
2025-06-09 16:31:12 +0900

    feat: favicon & title 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <title>Tech_won</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
