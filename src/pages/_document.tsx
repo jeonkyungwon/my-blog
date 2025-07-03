8b64f0198bffad9d73b15a898c1755cf3f3580da
jeonkyungwon <ashjkw3218@gmail.com>
2025-07-03 16:05:07 +0900

    feat: SEO 인증용 meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
           rel="stylesheet"
         />
+        <meta
+          name="google-site-verification"
+          content="Aybd-Sw4kmIAOgZ0EnbIqskK_dK0wR7UomChlxFcCT0"
+        />
       </Head>
       <body>
         <Main />
